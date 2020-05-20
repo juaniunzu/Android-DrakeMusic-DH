eb8fc75103d1ad2ed86b6c2b1c511cdda1f476ef
Lucho <luchobonisconti@gmail.com>
2020-05-20 17:17:03 -0300

    Se agrego la Funcionalidad del SplashScreen.
    Tiene 2 segundos para intercambiar

---
+++ app/src/main/java/com/example/projectointegrador/view/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.example.projectointegrador.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import com.example.projectointegrador.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private final int DURACION_DEL_SPLASH = 2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            };
+        }, DURACION_DEL_SPLASH);
+    }
+}
