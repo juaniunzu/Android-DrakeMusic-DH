ffbf863436c59853688c5b013ca96e7e5aef1900
Juani Unzurrunzaga <juanunzu@gmail.com>
2020-06-03 22:12:37 -0300

    Modificaciones en models

--- app/src/main/java/com/example/projectointegrador/model/Album.java
+++ app/src/main/java/com/example/projectointegrador/model/Album.java
@@ -2,26 +2,52 @@ package com.example.projectointegrador.model;
 
 import com.example.projectointegrador.R;
 import com.example.projectointegrador.util.Utils;
+import com.google.gson.annotations.SerializedName;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class Album implements Serializable, Utils.Searchable {
     private Integer id;
     private String title;
     private String cover;
     private Artist artist;
+    @SerializedName("genre_id")
+    private Integer genreId;
+    private List<Genre> genres;
+    private List<Track> tracks;
 
+    //constructor solo usado para crear lista hardcodeada de ultimas reproducciones. Borrar cuando se empiece a traer la data de otro lado
     public Album(Integer id, String title, String cover) {
         this.id = id;
         this.title = title;
         this.cover = cover;
     }
 
-    public Album(Integer id, String title, String cover, Artist artist) {
+    public Album(Integer id, String title, String cover, Artist artist, Integer genreId, List<Genre> genres, List<Track> tracks) {
         this.id = id;
         this.title = title;
         this.cover = cover;
         this.artist = artist;
+        this.genreId = genreId;
+        this.genres = genres;
+        this.tracks = tracks;
+    }
+
+    public List<Track> getTracks() {
+        return tracks;
+    }
+
+    public void setTracks(List<Track> tracks) {
+        this.tracks = tracks;
+    }
+
+    public List<Genre> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<Genre> genres) {
+        this.genres = genres;
     }
 
     public Album() {
---
+++ app/src/main/java/com/example/projectointegrador/model/Genre.java
@@ -0,0 +1,41 @@
+package com.example.projectointegrador.model;
+
+public class Genre {
+
+    private Integer id;
+    private String name;
+    private String picture;
+
+    public Genre(Integer id, String name, String picture) {
+        this.id = id;
+        this.name = name;
+        this.picture = picture;
+    }
+
+    public Genre() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+}
