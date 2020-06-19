7662e74c028d6fc996572aac386ea510a03581b6
Juani Unzurrunzaga <juanunzu@gmail.com>
2020-06-19 15:26:57 -0300

    Se agrega funcionalidad play/pause

--- app/src/main/java/com/example/projectointegrador/view/PlayerActivity.java
+++ app/src/main/java/com/example/projectointegrador/view/PlayerActivity.java
@@ -12,24 +12,32 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.projectointegrador.R;
 import com.example.projectointegrador.databinding.ActivityPlayerBinding;
 import com.example.projectointegrador.model.Track;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PlayerActivity extends AppCompatActivity {
+public class PlayerActivity extends AppCompatActivity implements PlayerFragment.PlayerFragmentListener {
 
     public static final String KEY_TRACK = "track";
     public static final String KEY_LISTA = "lista";
 
     private ViewPager viewPager;
     private Toolbar toolbar;
+    private MediaPlayer audioPlayer;
+    private ArrayList<Track> trackArrayList;
     private ActivityPlayerBinding binding;
 
 
@@ -54,7 +62,7 @@ public class PlayerActivity extends AppCompatActivity {
         Intent desdeMain = getIntent();
         Bundle datosDesdeMain = desdeMain.getExtras();
         Track trackClickeado = (Track) datosDesdeMain.getSerializable(KEY_TRACK);
-        ArrayList<Track> trackArrayList = (ArrayList<Track>) datosDesdeMain.getSerializable(KEY_LISTA);
+        trackArrayList = (ArrayList<Track>) datosDesdeMain.getSerializable(KEY_LISTA);
         List<Fragment> listaFragments = generarFragments(trackArrayList);
 
         Integer indice = trackArrayList.indexOf(trackClickeado);
@@ -65,12 +73,58 @@ public class PlayerActivity extends AppCompatActivity {
 
         viewPager.setCurrentItem(indice);
 
+        audioPlayer = new MediaPlayer();
+        audioPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        prepararTrackParaReproduccion(viewPager.getCurrentItem());
+        audioPlayer.start();
+
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if(audioPlayer != null){
+                    if(audioPlayer.isPlaying()){
+                        audioPlayer.stop();
+                    }
+                    audioPlayer.reset();
+                    audioPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                    prepararTrackParaReproduccion(position);
+                    audioPlayer.start();
+                }
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+
+
     }
+
+    private void prepararTrackParaReproduccion(Integer ordenTrackEnLista){
+        Track track = this.trackArrayList.get(ordenTrackEnLista);
+        try {
+            audioPlayer.setDataSource(this, Uri.parse(track.getPreview()));
+            audioPlayer.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     private List<Fragment> generarFragments(List<Track> listaDeTracks){
         List<Fragment> listaADevolver = new ArrayList<>();
         for (Track track : listaDeTracks) {
-            Fragment fragment = PlayerFragment.crearPlayerFragment(track);
+            Fragment fragment = PlayerFragment.crearPlayerFragment(track, this);
             listaADevolver.add(fragment);
         }
         return listaADevolver;
@@ -90,5 +144,56 @@ public class PlayerActivity extends AppCompatActivity {
             onBackPressed();
         }
         return true;
+    }
+
+
+   @Override
+    public void onClickPlay(ImageView botonPlay, ImageView botonPause) {
+        if(audioPlayer == null){
+            audioPlayer = new MediaPlayer();
+            audioPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            prepararTrackParaReproduccion(viewPager.getCurrentItem());
+        }
+        audioPlayer.start();
+        botonPlay.setVisibility(View.GONE);
+        botonPause.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onClickPause(ImageView botonPause, ImageView botonPlay) {
+        audioPlayer.pause();
+        botonPause.setVisibility(View.GONE);
+        botonPlay.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onClickNext() {
+
+    }
+
+    @Override
+    public void onClickPrevious() {
+
+    }
+
+    @Override
+    public void onClickShuffle(ImageView boton) {
+
+    }
+
+    @Override
+    public void onClickRepeat(ImageView boton) {
+
+    }
+
+    @Override
+    public void onClickAddFavorite(ImageView boton) {
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //audioPlayer.release();
     }
 }
--- app/src/main/java/com/example/projectointegrador/view/PlayerFragment.java
+++ app/src/main/java/com/example/projectointegrador/view/PlayerFragment.java
@@ -1,6 +1,9 @@
 package com.example.projectointegrador.view;
 
 import android.graphics.Bitmap;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
@@ -14,6 +17,8 @@ import com.example.projectointegrador.model.Track;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
+import java.io.IOException;
+
 import static com.example.projectointegrador.util.Utils.setFragmentBackground;
 
 
@@ -22,23 +27,28 @@ public class PlayerFragment extends Fragment {
     public static final String KEY_DETAIL_TRACK = "track";
     private FragmentPlayerBinding binding;
     private ImageView fragmentPlayerImageView;
-    private FloatingActionButton fragmentPlayerButtonNext;
-    private FloatingActionButton fragmentPlayerButtonPlay;
-    private FloatingActionButton fragmentPlayerButtonPrevious;
-    private FloatingActionButton fragmentPlayerButtonRepeat;
-    private FloatingActionButton fragmentPlayerButtonShuffle;
+    private ImageView fragmentPlayerButtonNext;
+    private ImageView fragmentPlayerButtonPlay;
+    private ImageView fragmentPlayerButtonPrevious;
+    private ImageView fragmentPlayerButtonRepeat;
+    private ImageView fragmentPlayerButtonShuffle;
+    private ImageView fragmentPlayerButtonPause;
     private ImageView fragmentPlayerButtonAddFavorite;
     private TextView fragmentPlayerTextViewArtista;
     private TextView fragmentPlayerTextViewNombre;
     private Bitmap bitmap;
+    private PlayerFragmentListener listener;
+    private Track trackRecibido;
+
 
 
     /**
      * Nuevo constructor. Setea el fragment segun el {@param track} parametro
      * @return
      */
-    public static PlayerFragment crearPlayerFragment(Track track){
+    public static PlayerFragment crearPlayerFragment(Track track, PlayerFragmentListener listener){
         PlayerFragment fragment = new PlayerFragment();
+        fragment.setListener(listener);
         Bundle datosDeTrack = new Bundle();
         datosDeTrack.putSerializable(KEY_DETAIL_TRACK, track);
         fragment.setArguments(datosDeTrack);
@@ -61,18 +71,75 @@ public class PlayerFragment extends Fragment {
         final View view = binding.getRoot();
 
         Bundle desdeMain = getArguments();
-        final Track trackRecibido = (Track) desdeMain.getSerializable(KEY_DETAIL_TRACK);
+        trackRecibido = (Track) desdeMain.getSerializable(KEY_DETAIL_TRACK);
 
         setViewResources(trackRecibido);
 
         setFragmentBackground(getContext(), view, trackRecibido.getAlbum().getCover());
 
+        fragmentPlayerButtonPlay.setVisibility(View.GONE);
+
+        fragmentPlayerButtonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.onClickPlay(fragmentPlayerButtonPlay, fragmentPlayerButtonPause);
+            }
+        });
+
+        fragmentPlayerButtonPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.onClickPause(fragmentPlayerButtonPause, fragmentPlayerButtonPlay);
+            }
+        });
+
+        fragmentPlayerButtonAddFavorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.onClickAddFavorite(fragmentPlayerButtonAddFavorite);
+            }
+        });
+
+        fragmentPlayerButtonRepeat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.onClickRepeat(fragmentPlayerButtonRepeat);
+            }
+        });
+
+        fragmentPlayerButtonNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.onClickNext();
+            }
+        });
+
+        fragmentPlayerButtonPrevious.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.onClickPrevious();
+            }
+        });
+
+        fragmentPlayerButtonShuffle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.onClickShuffle(fragmentPlayerButtonShuffle);
+            }
+        });
+
+
+
         return view;
     }
 
+    public void setListener(PlayerFragmentListener listener) {
+        this.listener = listener;
+    }
 
-
-
+    public Track getTrackRecibido() {
+        return trackRecibido;
+    }
 
     private void setViewResources(Track trackRecibido) {
         Glide.with(getContext()).load(trackRecibido.getAlbum().getCover()).into(fragmentPlayerImageView);
@@ -85,11 +152,27 @@ public class PlayerFragment extends Fragment {
         fragmentPlayerButtonAddFavorite = binding.fragmentPlayerButtonAddFavorite;
         fragmentPlayerTextViewArtista = binding.fragmentPlayerTextViewArtista;
         fragmentPlayerTextViewNombre = binding.fragmentPlayerTextViewNombre;
+        fragmentPlayerButtonPause = binding.fragmentPlayerButtonPause;
+        fragmentPlayerButtonPlay = binding.fragmentPlayerButtonPlay;
+        fragmentPlayerButtonNext = binding.fragmentPlayerButtonNext;
+        fragmentPlayerButtonPrevious = binding.fragmentPlayerButtonPrevious;
+        fragmentPlayerButtonRepeat = binding.fragmentPlayerButtonRepeat;
+        fragmentPlayerButtonShuffle = binding.fragmentPlayerButtonShuffle;
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
         binding = null;
+    }
+
+    public interface PlayerFragmentListener{
+        void onClickPlay(ImageView botonPlay, ImageView botonPause);
+        void onClickPause(ImageView botonPause, ImageView botonPlay);
+        void onClickNext();
+        void onClickPrevious();
+        void onClickShuffle(ImageView boton);
+        void onClickRepeat(ImageView boton);
+        void onClickAddFavorite(ImageView boton);
     }
 }
