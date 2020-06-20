7fb08a9865a8b4002f4900166535101f63c596e7
Sebastian <paredessebastian013@gmail.com>
2020-06-20 17:45:58 -0300

    Add Login Firebase (mail and pass)

--- app/src/main/java/com/example/projectointegrador/view/LoginActivity.java
+++ app/src/main/java/com/example/projectointegrador/view/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.projectointegrador.view;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -7,10 +8,15 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.projectointegrador.R;
@@ -27,20 +33,31 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity implements LoginFragment.LoginFragmentListener,
         LoginInicioFragment.LoginInicioFragmentListener, SignUpFragment.SignUpFragmentListener {
 
+    private static final String TAG = "FIREBASE";
     private static final String EMAIL = "email";
     private static final int RC_SIGN_IN = 1;
     private GoogleSignInClient mGoogleSignInClient;
     private CallbackManager callbackManager;
     private ActivityLoginBinding binding;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +65,7 @@ public class LoginActivity extends AppCompatActivity implements LoginFragment.Lo
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
-
+        getHashkey();
 
 
         Glide.with(this).asGif().load("http://s7.gifyu.com/images/gifFondoLogin.gif").into(binding.imagenFondo);
@@ -58,6 +75,7 @@ public class LoginActivity extends AppCompatActivity implements LoginFragment.Lo
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
 
@@ -65,14 +83,18 @@ public class LoginActivity extends AppCompatActivity implements LoginFragment.Lo
 
         callbackManager = CallbackManager.Factory.create();
 
+        mAuth = FirebaseAuth.getInstance();
+
 
     }
-    private void pegarFragmentInicial(Fragment fragment){
+
+    private void pegarFragmentInicial(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.add(R.id.activityLogin_fragmentContainer, fragment);
         fragmentTransaction.commit();
     }
+
     private void pegarFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
@@ -92,6 +114,63 @@ public class LoginActivity extends AppCompatActivity implements LoginFragment.Lo
         //Actualizar la UI
         updateUIGoogle(account);
         updateUIFacebook(isLoggedInOnFacebook());
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUIFirebase(currentUser);
+    }
+
+    private void updateUIFirebase(FirebaseUser currentUser) {
+        if (currentUser != null){
+            pasarALaMainActivityMatandoActividadActual();
+        }
+    }
+
+    private void loginFirebaseUser(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUIFirebase(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUIFirebase(null);
+                            // ...
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    private void createFirebaseUser(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUIFirebase(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUIFirebase(null);
+                        }
+
+                        // ...
+                    }
+                });
+
     }
 
     private void updateUIGoogle(GoogleSignInAccount account) {
@@ -142,7 +221,7 @@ public class LoginActivity extends AppCompatActivity implements LoginFragment.Lo
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
         try {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-
+            Log.d(TAG, "firebaseAuthWithGoogle:" + account.getId());
             // Signed in successfully, show authenticated UI.
             updateUIGoogle(account);
         } catch (ApiException e) {
@@ -167,8 +246,7 @@ public class LoginActivity extends AppCompatActivity implements LoginFragment.Lo
 
     @Override
     public void onClickLoginFragmentBotonLogin(String username, String password) {
-        //cuando tengamos firebase authentication, se toca este modulo
-        pasarALaMainActivityMatandoActividadActual();
+        loginFirebaseUser(username, password);
     }
 
     @Override
@@ -214,26 +292,32 @@ public class LoginActivity extends AppCompatActivity implements LoginFragment.Lo
 
     @Override
     public void onClickSignUpFragmentBotonRegistrarse(String username, String password) {
-        //editar, esto es crear cuenta con datos, cuando tengamos firebase authentication, se toca este modulo
-        pasarALaMainActivityMatandoActividadActual();
+        if (username.contains(" ") && !username.contains("@") && (!username.contains(".com") || !username.contains(".net") || !username.contains(".org"))) {
+            Toast.makeText(this, "El Email no es Valido", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (password.length() < 6){
+            Toast.makeText(this, "La contraseña debe tener al menos 6 caracteres", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        createFirebaseUser(username, password);
     }
 
-//    Metodo para conseguir la hashkey.
-//    public void getHashkey() {
-//        try {
-//            PackageInfo info = getPackageManager().getPackageInfo(
-//                    "com.example.projectointegrador",
-//                    PackageManager.GET_SIGNATURES);
-//            for (Signature signature : info.signatures) {
-//                MessageDigest md = MessageDigest.getInstance("SHA");
-//                md.update(signature.toByteArray());
-//                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-//            }
-//        } catch (PackageManager.NameNotFoundException e) {
-//
-//        } catch (NoSuchAlgorithmException e) {
-//
-//        }
-//    }
-}
+    //    Metodo para conseguir la hashkey.
+    public void getHashkey() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.example.projectointegrador",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
 
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+    }
+}
