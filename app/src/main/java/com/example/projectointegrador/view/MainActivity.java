26c503168f1820f607bdb5635eb63fdbbfeea337
Lucho <luchobonisconti@gmail.com>
2020-07-13 15:23:52 -0300

    Chequea si hay internet en el Shake del Main Activity

--- app/src/main/java/com/example/projectointegrador/view/MainActivity.java
+++ app/src/main/java/com/example/projectointegrador/view/MainActivity.java
@@ -748,7 +748,9 @@ ShakeDetector.Listener{
 
     @Override
     public void hearShake() {
-
+        if(!Utils.hayInternet(this)){
+            return;
+        }
         if(drakePlayer.getTrackList() != null && !drakePlayer.getTrackList().isEmpty()){
             int cantTemas = drakePlayer.getTrackList().size();
             Random r = new Random();
